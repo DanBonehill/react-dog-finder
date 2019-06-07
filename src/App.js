3a03990f600859dc646bfeeb42dd9d072c8da8fd
DanBonehill <DanBonehill@me.com>
2019-06-07 14:11:29 +0100

    Create DogDetails Component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import { Switch, Route } from "react-router-dom";
 import DefaultProps from "./DefaultProps";
 import "./App.css";
 import DogList from "./DogList";
+import DogDetails from "./DogDetails";
 
 class App extends Component {
   static defaultProps = DefaultProps;
 
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
+
     return (
-      <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+      <Switch>
+        <Route
+          exact
+          path="/dogs"
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path="/dogs/:name" render={getDog} />
+      </Switch>
     );
   }
 }
