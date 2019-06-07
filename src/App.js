87479ac6b5c4e34a9e2e9f648c73595073d601fc
DanBonehill <DanBonehill@me.com>
2019-06-07 14:44:15 +0100

    Refactor routes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
 import DefaultProps from "./DefaultProps";
 import "./App.css";
-import DogList from "./DogList";
-import DogDetails from "./DogDetails";
 import Navbar from "./Navbar";
+import Routes from "./Routes";
 
 class App extends Component {
   static defaultProps = DefaultProps;
 
   render() {
-    const getDog = props => {
-      let name = props.match.params.name;
-      let currentDog = this.props.dogs.find(
-        dog => dog.name.toLowerCase() === name.toLowerCase()
-      );
-      return <DogDetails {...props} dog={currentDog} />;
-    };
-
     return (
       <div>
         <Navbar dogs={this.props.dogs} />
-        <Switch>
-          <Route
-            exact
-            path="/dogs"
-            render={() => <DogList dogs={this.props.dogs} />}
-          />
-          <Route exact path="/dogs/:name" render={getDog} />
-        </Switch>
+        <div className="container">
+          <Routes dogs={this.props.dogs} />
+        </div>
       </div>
     );
   }
